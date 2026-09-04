b96d791a0d338e4f9f0cb806ffa0308198b7e7ae
Yuto Oshima <yuto.oshima814@gmail.com>
2026-09-04 14:55:06 +0900

    feat: Fortifyの実装(一般ユーザログイン)

--- config/app.php
+++ config/app.php
@@ -3,6 +3,7 @@
 use App\Providers\AppServiceProvider;
 use App\Providers\AuthServiceProvider;
 use App\Providers\EventServiceProvider;
+use App\Providers\FortifyServiceProvider;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
@@ -172,6 +173,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         EventServiceProvider::class,
         RouteServiceProvider::class,
+        FortifyServiceProvider::class, 
     ])->toArray(),
 
     /*
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/login', function () {
+    return view('admin.admin-login');
+})->name('admin.login');
